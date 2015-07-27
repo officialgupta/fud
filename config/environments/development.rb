5fe93207d212a618058e5f8f9074b9d39cc17a76
Rails Developer <railsdeveloper@Nicolas-MacBook-Pro-2.local>
2015-07-27 14:19:23 +0100

    Add users (using HAML)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,11 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Devise
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.assets.initialize_on_precompile = false
+
 end
