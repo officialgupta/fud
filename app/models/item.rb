ec7fa98bd7c7519112c61b947e7c8d9f4cd3e5b0
Rails Developer <railsdeveloper@Nicolas-MacBook-Pro-2.local>
2015-07-27 16:14:50 +0100

    Add when expire to item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ActiveRecord::Base
   belongs_to :user
   belongs_to :food
+
+  before_validation :set_when_expire
+
+  def set_when_expire
+    self.when_expire = self.when_bought + self.food.time_to_expire_in_days.days
+  end
 end
