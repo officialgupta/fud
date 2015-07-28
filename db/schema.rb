2a62521d3b5a50eb56d97528cb0407ec777f81b7
Neena <junkemail273@gmail.com>
2015-07-28 11:47:13 +0100

    Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727145746) do
+ActiveRecord::Schema.define(version: 20150727150201) do
 
   create_table "foodbanks", force: :cascade do |t|
     t.string   "company"
@@ -23,5 +23,44 @@ ActiveRecord::Schema.define(version: 20150727145746) do
     t.float    "latitude"
     t.float    "longitude"
   end
+
+  create_table "foods", force: :cascade do |t|
+    t.string   "name"
+    t.string   "food_type"
+    t.integer  "time_to_expire_in_days"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.decimal  "quantity"
+    t.string   "quantity_type"
+    t.date     "when_bought"
+    t.date     "when_expire"
+    t.integer  "user_id"
+    t.integer  "food_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "status"
+    t.string   "where_stored"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
