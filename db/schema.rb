fd4a2f049ed1515a077c86a827e22d40c0e405b3
Neena <junkemail273@gmail.com>
2015-07-29 18:05:08 +0100

    Schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729125622) do
+ActiveRecord::Schema.define(version: 20150729154156) do
 
   create_table "foodbanks", force: :cascade do |t|
     t.string   "company"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150729125622) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.string   "where_stored"
+    t.string   "image_url"
   end
 
   create_table "foods_recipes", force: :cascade do |t|
@@ -59,6 +60,16 @@ ActiveRecord::Schema.define(version: 20150729125622) do
     t.string   "quantity_type"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "recipes", force: :cascade do |t|
