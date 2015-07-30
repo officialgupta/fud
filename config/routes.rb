f042aac4e7ebd564ec36743b04c9437e2c32b216
Rails Developer <railsdeveloper@Nicolas-MacBook-Pro-2.local>
2015-07-30 11:51:41 +0100

    Add user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   root 'pages#index'
 
   resources :photos
@@ -14,6 +16,7 @@ Rails.application.routes.draw do
   get '/scan' => 'scans#scan', :as => "scan"
 
   devise_for :users
+  get "/profile/:id" => 'users#show', :as => "profile"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
