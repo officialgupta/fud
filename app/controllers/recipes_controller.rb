ee5b8dc2a4484b17fd2f6005a7efd69dd017af44
Neena <junkemail273@gmail.com>
2015-07-30 15:25:51 +0100

    Some work on pulling recipes in

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,11 @@
 class RecipesController < ApplicationController
-    def index
-
-    end
+  require 'HTTParty'
+  require 'uri'
+  def index
+    @ingredients = params[:ingredients]||current_user.items.in_use.map(&:food)
+    @search_ingredients = @ingredients.map {|i| i.name.split.first}.join(",").encode('UTF-8', :invalid => :replace) || "tomato"
+    @page = params[:page] || 1
+    response = HTTParty.get("http://api.pearson.com:80/kitchen-manager/v1/recipes?ingredients-any=#{URI.encode(@search_ingredients)}&offset=#{(@page-1)*20}&limit=20")
+    @recipes = response.parsed_response["results"].map{|r| OpenStruct.new(r)}
+  end
 end
