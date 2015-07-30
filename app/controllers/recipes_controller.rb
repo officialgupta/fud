12ac09b6d7053b035b9c3a1060ca43aebf9412bd
Neena <junkemail273@gmail.com>
2015-07-30 16:11:53 +0100

    Recipes can now be viewed. It looks not great but okay

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,10 +2,26 @@ class RecipesController < ApplicationController
   require 'HTTParty'
   require 'uri'
   def index
-    @ingredients = params[:ingredients]||current_user.items.in_use.map(&:food)
+    if params[:ingredients].nil? && current_user.items.in_use.count != 0
+      @ingredients = current_user.items.in_use.map(&:food)
+    elsif !params[:ingredients].nil?
+      @ingredients = params[:ingredients].map{|id| Food.find(id)}
+    else
+      @ingredients = []      
+    end
+
     @search_ingredients = @ingredients.map {|i| i.name.split.first}.join(",").encode('UTF-8', :invalid => :replace) || "tomato"
-    @page = params[:page] || 1
+    @page = params[:page].to_i
+    @page = 1 if @page == 0
+
     response = HTTParty.get("http://api.pearson.com:80/kitchen-manager/v1/recipes?ingredients-any=#{URI.encode(@search_ingredients)}&offset=#{(@page-1)*20}&limit=20")
     @recipes = response.parsed_response["results"].map{|r| OpenStruct.new(r)}
+
+    @pages = (response.parsed_response["total"]/20.0).ceil
+  end
+
+  def show
+    response = HTTParty.get("https://api.pearson.com/kitchen-manager/v1/recipes/#{params[:id]}")
+    @recipe = OpenStruct.new(response)
   end
 end
