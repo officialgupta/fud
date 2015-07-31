c0869181a9aaa7c5c018876fdb74409825a48cf8
Rails Developer <railsdeveloper@Nicolas-MacBook-Pro-2.local>
2015-07-31 16:40:11 +0100

    Fix controller

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -1,12 +1,13 @@
 class ScansController < ApplicationController
 
   def scan
-    Item.create!(:food_id => Food.from_ean(sms_params[:content]).id, :quantity => 1, :quantity_type => "units", :when_bought => Date.today, :user_id => user_id)
+    Item.create!(:food_id => Food.from_ean(sms_params[:content].tr('/', '')).id, :quantity => 1, :quantity_type => "units", :when_bought => Date.today, :user_id => user_id)
+    puts Food.from_ean(sms_params[:content].tr('/', '')).name
   end
 
   private
 
   def sms_params
-    params.require(:scans).permit(:content)
+    params.permit(:content)
   end
 end
