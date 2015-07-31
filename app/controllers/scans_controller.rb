bb9cc7d60026133dd7fcd051b2b0184e22feb944
Rails Developer <railsdeveloper@Nicolas-MacBook-Pro-2.local>
2015-07-31 17:12:33 +0100

    Fix

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -1,7 +1,7 @@
 class ScansController < ApplicationController
 
   def scan
-    Item.create!(:food_id => Food.from_ean(sms_params[:content].tr('/', '')).id, :quantity => 1, :quantity_type => "units", :when_bought => Date.today, :user_id => current_user)
+    Item.create!(:food_id => Food.from_ean(sms_params[:content].tr('/', '')).id, :quantity => 1, :quantity_type => "units", :when_bought => Date.today, :user_id => current_user.id)
     redirect_to root_path, notice: 'Item was successfully scanned.'
   end
 
